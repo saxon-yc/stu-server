27588d8d3f5fb98f3619be6a9f7a042a061f1ff9
sishenghuang <sishenghuang@yunify.com>
2024-07-10 14:03:05 +0800

    feat: 国际化

---
+++ internal/model/constants.go
@@ -0,0 +1,6 @@
+package model
+
+const (
+	Language_zh string = "zh-cn"
+	Language_en string = "en"
+)
--- locales/translation.go
+++ locales/translation.go
@@ -1,39 +1,59 @@
 package locales
 
 import (
+	_ "embed"
 	"log"
+	"student-server/internal/model"
 
 	"github.com/BurntSushi/toml"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 )
 
-var bundle *i18n.Bundle
-var localizer *i18n.Localizer
+var (
+	bundle    *i18n.Bundle
+	localizer *i18n.Localizer
+
+	//go:embed active.en.toml
+	localesENFiles []byte
+
+	//go:embed active.zh.toml
+	localesZHFiles []byte
+)
 
 func init() {
 	// 创建一个新的 Bundle
 	bundle = i18n.NewBundle(language.English)
 	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+}
 
-	// 加载翻译文件
-	_, err := bundle.LoadMessageFile("locales/active.en.toml")
-	if err != nil {
-		log.Fatalf("failed to load message file: %v", err)
+// 加载翻译文件
+func loadLanguageFile(lang string) {
+	switch lang {
+	default:
+	case model.Language_zh:
+		if _, err := bundle.ParseMessageFileBytes(localesZHFiles, "locales/active.zh.toml"); err != nil {
+			log.Fatalf("Failed to parse zh_locales message file: %v", err)
+		}
+	case model.Language_en:
+		if _, err := bundle.ParseMessageFileBytes(localesENFiles, "locales/active.en.toml"); err != nil {
+			log.Fatalf("Failed to parse en_locales message file: %v", err)
+		}
 	}
-	_, err = bundle.LoadMessageFile("locales/active.zh.toml")
-	if err != nil {
-		log.Fatalf("failed to load message file: %v", err)
+
+}
+func setLanguageEnv(lang string) {
+	switch lang {
+	default:
+	case model.Language_zh:
+		localizer = i18n.NewLocalizer(bundle, "zh")
+	case model.Language_en:
+		localizer = i18n.NewLocalizer(bundle, "en")
 	}
 }
 func NewLocalizer(lang string) {
-	switch lang {
-	default:
-	case "zh-cn":
-		localizer = i18n.NewLocalizer(bundle, "zh")
-	case "en":
-		localizer = i18n.NewLocalizer(bundle, "en")
-	}
+	loadLanguageFile(lang)
+	setLanguageEnv(lang)
 }
 
 func GetTranslatedMessage(messageID string) string {
